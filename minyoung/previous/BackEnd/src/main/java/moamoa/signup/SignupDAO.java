afe27ac4db9b4a432c4d495e24664fbc7b469926
Cat Hanbit <dreamlmy1203@naver.com>
2022-08-12 17:44:40 +0900

    이민영-DAO,DTO,jsp 파일 커밋
    모든 코드를 고침
    DB 연결 코드도 마찬가지
    완료

--- minyoung/previous/BackEnd/src/main/java/moamoa/signup/SignupDAO.java
+++ minyoung/previous/BackEnd/src/main/java/moamoa/signup/SignupDAO.java
@@ -1,84 +1,136 @@
 package moamoa.signup;
-/*DAO: Data 처리
-DAO(Data Access Object)란 데이터베이스에 접속해서 데이터 수정, 삭제, 추가 등의 작업을 하는 클래스입니다.
-일반적인 JSP 혹은 Servlet 페이지 내에서 위의 로직들을 함께 쓸 수 있지만 유지보수 및 코드의 모듈화를 위해 DAO라는 클래스를 따로 만들어 사용합니다.
-DTO란 DAO클래스를 이용하여 데이터베이스에서 데이터를 관리할 때 데이터를 일반적인 변수에 저장하여 사용할 수 있지만 해당 데이터의 클래스를 만들어 사용합니다.
-
-*/
 
 import java.sql.*;
 import java.util.*;
 
 public class SignupDAO {
+
+	String driver="oracle.jdbc.driver.OracleDriver";
+	String url="jdbc:oracle:thin:@localhost:1521:XE";
 	
-	String driver="oracle.jdbc.driver.OracleDriver"; //DB 켠다.
-	String url="jdbc:oracle:thin:@localhost:1521:XE"; // DB 연결=connection
-	
-	//생성자
 	public SignupDAO() {
 		try {
-			Class.forName(driver);
-			System.out.println("오라클드라이버 성공");
-		} catch (ClassNotFoundException e) {
+				Class.forName(driver);
+				System.out.println("오라클드라이버 성공");
+		} catch(ClassNotFoundException e){
+			// TODO Auto-generated catch block
 			System.out.println("오라클드라이버 실패");
 		}
 	}
-	
-	////////connection
-	public Connection getConnection()
-	{		Connection conn=null;
+	/////////////connection
+	public Connection getConnection() {
+		Connection conn = null;
 		
 		try {
 			conn=DriverManager.getConnection(url, "end", "end");
-		} catch (SQLException e) {
+		}catch(SQLException e) {
+			// TODO Auto-generated catch block
 			System.out.println("커넥션실패");
 		}
+		
 		return conn;
 	}
-	
-	//////insert
-	public void SignupInsert(SignupDTO dto)
-	{
-		Connection conn = null;
+	////////////insert
+	public void signuplnsert(SignupDTO dto) { // 회원가입
+		
+		Connection conn=null;
 		PreparedStatement pstmt = null;
 		
 		String sql = "insert INTO MEMBER(id, pwd, name, phone_num, email) values(?, ?, ?, ?, ?)";
 		
-		conn = getConnection();
+		conn=getConnection();
 		try {
 			pstmt=conn.prepareStatement(sql);
-			//??에 대한 바인딩
-			pstmt.setString(1/*물음표 안에 들어가는 순서*/, dto.getJoin_id());
+			
+			pstmt.setString(1, dto.getJoin_id());
 			pstmt.setString(2, dto.getJoin_pw());
 			pstmt.setString(3, dto.getJoin_name());
 			pstmt.setString(4, (dto.getJoin_cell1() +"-"+ dto.getJoin_cell2() +"-"+ dto.getJoin_cell3()));
-			pstmt.setString(5, dto.getJoin_email0() +"@"+ dto.getJoin_email1());
-			
-		
-			
-			//update
+			pstmt.setString(5, (dto.getJoin_email0() +"@"+ dto.getJoin_email1()));
 			pstmt.executeUpdate();
-		} catch (SQLException e) {
+			
+		}catch(SQLException e) {
 			e.printStackTrace();
-		} finally {
+		}finally {
 			try {
 				pstmt.close();
 				conn.close();
-			} catch (SQLException e) {
+			}catch(SQLException e) {
 				e.printStackTrace();
 			}
 		}
 		
 	}
+	////////////select
+	public Vector<SignupDTO> getAllMemberDatas(){
+		Vector<SignupDTO> list = new Vector<SignupDTO>();
+		
+		Connection conn=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs = null;
+		
+		String sql = "select * from MEMBER order by num asc";
+		
+		conn=getConnection();
+		try {
+			pstmt=conn.prepareStatement(sql);
+			rs=pstmt.executeQuery();
+			
+			while(rs.next()) {
+				SignupDTO dto = new SignupDTO();
+				
+				dto.setJoin_id(rs.getString("id"));
+				dto.setJoin_pw(rs.getString("pwd"));
+				dto.setJoin_name(rs.getString("name"));
+				
+				list.add(dto);
+				
+			}
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				rs.close();
+				pstmt.close();
+				conn.close();
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return list;
+	}
 	
-	
+	public boolean loginCheckId(String id) {
+		Connection conn=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs = null;
+		String sql = "select id from MEMBER where id=?";
+		
+		conn=getConnection();
+		try {
+			pstmt=conn.prepareStatement(sql);
+			
+			pstmt.setString(1, id);
+			rs=pstmt.executeQuery();
+			if(rs.next()) {
+				return true;
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				pstmt.close();
+				conn.close();
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return false;
+	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		
-		
-
+		SignupDAO dao = new SignupDAO();
 	}
-
+	
 }
